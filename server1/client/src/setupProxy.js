a7cb3be13849ca7701cd9b5a72d1a7be548006f6
arnabchat123 <68984466+arnabchat123@users.noreply.github.com>
2021-06-17 11:17:20 +0530

    Delete server1 directory

--- server1/client/src/setupProxy.js
+++
@@ -1,9 +0,0 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
-module.exports = function (app) {
-  app.use(
-    ['/api'], //, '/auth/github', '/auth/facebook'],
-    createProxyMiddleware({
-      target: 'http://localhost:5678',
-    })
-  );
-};
