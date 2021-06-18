3cc564003c9c8e4009ab40cc32d574d9734495f6
Arnab Chatterjee028 <arnacha1@in.ibm.com>
2021-06-18 09:15:32 +0530

    v1.0.16

--- server.js
+++ server.js
@@ -17,9 +17,10 @@ var objectStore = require('./objectStorage');
 
 app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
   res.header(
     'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
+    'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json'
   );
   next();
 });
